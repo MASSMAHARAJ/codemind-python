b06bedabb29189254bd5ecec215a7b1e4ed3fea7
SRI KARTHEEK <97425080+MASSMAHARAJ@users.noreply.github.com>
2022-05-04 11:57:39 +0530

    n=int(input()) alt=n for i in range(1,n+1):     alt=n     while alt!=0:         print(alt,end=' ')         alt-=1     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,9 +1,8 @@
 n=int(input())
-m=n+1
+alt=n
 for i in range(1,n+1):
-    for j in range(1,n+1):
-        if j==i :
-            print(0,end='')
-        else:
-            print("x",end='')
+    alt=n
+    while alt!=0:
+        print(alt,end=' ')
+        alt-=1
     print()
