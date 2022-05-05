115f94e46ecdf13853a0e25599df3fa0436c2cdd
SRI KARTHEEK <97425080+MASSMAHARAJ@users.noreply.github.com>
2022-05-05 11:57:20 +0530

    n=int(input()) k=2*n-1 k1=n k2=n for i in range(1,n+1):     for j in range(1,k2+1):         if j>=k1 and j<n:             print(m,end='')             m-=1         elif j>=n and j<=k2:             print(j-n,end='')         else:             print(" ",end='')     print()     k1-=1     k2+=1     m=i

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,18 @@
 n=int(input())
-alt=n
+k=2*n-1
+k1=n
+k2=n
 for i in range(1,n+1):
-    alt=n
-    while alt!=0:
-        print(alt,end=' ')
-        alt-=1
+    for j in range(1,k2+1):
+        if j>=k1 and j<n:
+            print(m,end='')
+            m-=1
+        elif j>=n and j<=k2:
+            print(j-n,end='')
+        else:
+            print(" ",end='')
     print()
+    k1-=1
+    k2+=1
+    m=i
+
