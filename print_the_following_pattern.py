c374d17d14fa240814068e7ed57a3e24f7316b50
SRI KARTHEEK <97425080+MASSMAHARAJ@users.noreply.github.com>
2022-07-14 11:44:30 +0530

    n=int(input())#4 k=2*n-1       #7 for i in range(0,k):     if i>=n-1:         print('*',end='')     else:         print(" ",end='') print() for i in range(0,n-2):#0,1     k-=1#6     for j in range(0,k):#0-5         if j==k-n or j==k-1:             print("*",end='')         else:             print(' ',end='')     print() for i in range(0,n):     print('*',end='') ''' 0123456    ****   *  *  *  * ****
    '''

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,13 +1,27 @@
-n=int(input())
-k=2*n-1
-k1=n
-k2=n
-for i in range(1,n+1):
-    for j in range(1,k2+1):
-        if j>=k1 and j<=k2:
-            print(i,end='')
+n=int(input())#4
+k=2*n-1       #7
+for i in range(0,k):
+    if i>=n-1:
+        print('*',end='')
+    else:
+        print(" ",end='')
+print()
+for i in range(0,n-2):#0,1
+    k-=1#6
+    for j in range(0,k):#0-5
+        if j==k-n or j==k-1:
+            print("*",end='')
         else:
-            print(" ",end='')
+            print(' ',end='')
     print()
-    k1-=1
-    k2+=1
+for i in range(0,n):
+    print('*',end='')
+'''
+0123456
+   ****
+  *  *
+ *  *
+****
+
+
+'''
